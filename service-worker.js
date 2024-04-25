3d8da7d3204c078b063907888c6e39a580b5c460
Ahmed Maher <ahmedmaheraljwhry057@gmail.com>
2024-04-25 18:33:59 +0200

    Some Edits

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
-  "/",
-  "/index.html"
-]
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = ["/", "/index.html"];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+self.addEventListener("install", (installEvent) => {
+	installEvent.waitUntil(
+		caches.open(staticDevCoffee).then((cache) => {
+			cache.addAll(assets);
+		})
+	);
+});
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+self.addEventListener("fetch", (fetchEvent) => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then((res) => {
+			return res || fetch(fetchEvent.request);
+		})
+	);
+});
